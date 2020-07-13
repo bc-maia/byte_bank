6f39b777cf22c1f83f3eb6666f3de2417106a218
bruno maia <bruno.maia@aec.com.br>
2020-07-13 15:55:04 -0300

    update - added transfer card

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+Color _green = Colors.green[800];
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.green[800], title: Text('Transferências')),
-        floatingActionButton:
-            FloatingActionButton(onPressed: null, child: Icon(Icons.add)),
-      ),
+          appBar:
+              AppBar(backgroundColor: _green, title: Text('Transferências')),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () => print('click cria nova transfer'),
+              backgroundColor: _green,
+              child: Icon(Icons.add)),
+          body: _ListaTransfers()),
+    );
+  }
+}
+
+class _ListaTransfers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+          elevation: 1.0,
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('R\$100'),
+            subtitle: Text('Transfer Description'),
+          ),
+        ),
+      ],
     );
   }
 }
