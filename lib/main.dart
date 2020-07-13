872710960d9ccdc90ce87c47472b10b9c01163e8
bruno maia <bruno.maia@aec.com.br>
2020-07-13 18:05:08 -0300

    refactoring - decoupling widget objects

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,49 @@ class MyApp extends StatelessWidget {
               onPressed: () => print('click cria nova transfer'),
               backgroundColor: _green,
               child: Icon(Icons.add)),
-          body: _ListaTransfers()),
+          body: TransferList()),
     );
   }
 }
 
-class _ListaTransfers extends StatelessWidget {
+class TransferList extends StatefulWidget {
+  @override
+  createState() => _TransferListState();
+}
+
+class _TransferListState extends State<TransferList> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: <Widget>[
-        Card(
-          elevation: 1.0,
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('R\$100'),
-            subtitle: Text('Transfer Description'),
-          ),
-        ),
+        TransferItem(Transfer(90.0, 'Transfer Description A')),
+        TransferItem(Transfer(200.20, 'Transfer Description B')),
+        TransferItem(Transfer(321.65, 'Transfer Description 3'))
       ],
     );
   }
+}
+
+class TransferItem extends StatelessWidget {
+  @required
+  final Transfer _transfer;
+
+  const TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 1.0,
+        child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('R\$${_transfer.value}'),
+            subtitle: Text(_transfer.description)));
+  }
+}
+
+class Transfer {
+  final double value;
+  final String description;
+
+  Transfer(this.value, this.description);
 }
