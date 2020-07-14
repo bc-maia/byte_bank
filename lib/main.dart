81da88e038507ce42f3c3a4f5d99732b4e239831
bruno maia <bruno.maia@aec.com.br>
2020-07-13 22:19:29 -0300

    updated - added transfer form

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(BankApp());
 
-Color _green = Colors.green[800];
+Color _greenTheme = Colors.green[800];
 
-class MyApp extends StatelessWidget {
+class BankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar:
-              AppBar(backgroundColor: _green, title: Text('Transferências')),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () => print('click - add new transfer entry'),
-              backgroundColor: _green,
-              child: Icon(Icons.add)),
-          body: TransferList()),
+      debugShowCheckedModeBanner: true,
+      home: TransferForm(),
+    );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(backgroundColor: _greenTheme, title: Text('Transfer Form')),
+      body: ListView(
+        children: <Widget>[
+          InputField(
+              label: 'Account Number',
+              hint: '000000',
+              icon: Icons.person,
+              type: TextInputType.number),
+          InputField(
+              label: 'Amount',
+              hint: 'R\$100.00',
+              icon: Icons.monetization_on,
+              type: TextInputType.number),
+          InputField(
+            label: 'Description',
+            hint: 'Mortgage/Rent',
+            icon: Icons.info,
+          ),
+          Center(
+            heightFactor: 2,
+            child: RaisedButton(
+              child: Text('Submit'),
+              color: _greenTheme,
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              onPressed: () => print('submit'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final IconData icon;
+  final TextInputType type;
+
+  const InputField({Key key, this.label, this.hint, this.icon, this.type})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: label, icon: Icon(icon) ?? null, hintText: hint),
+        keyboardType: type ?? TextInputType.text,
+        style: TextStyle(fontSize: 16.0),
+      ),
     );
   }
 }
@@ -28,12 +84,20 @@ class TransferList extends StatefulWidget {
 class _TransferListState extends State<TransferList> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TransferItem(Transfer(90.0, 'Transfer Description A')),
-        TransferItem(Transfer(200.20, 'Transfer Description B')),
-        TransferItem(Transfer(321.65, 'Transfer Description 3'))
-      ],
+    return Scaffold(
+      appBar:
+          AppBar(backgroundColor: _greenTheme, title: Text('Transferências')),
+      body: ListView(
+        children: <Widget>[
+          TransferItem(Transfer(90.0, 'Transfer Description A')),
+          TransferItem(Transfer(200.20, 'Transfer Description B')),
+          TransferItem(Transfer(321.65, 'Transfer Description 3'))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => print('click - add new transfer entry'),
+          backgroundColor: _greenTheme,
+          child: Icon(Icons.add)),
     );
   }
 }
